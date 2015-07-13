be567961571d6d885f3b8ca2e2c442ae564bd51e
Bob Lounsbury <blounsbury@usbr.gov>
2015-07-13 15:07:46 -0600

    Initial project setup, and .gitignore.

---
+++ BiasCorrectedToPisces.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace shop
+{
+class BiasCorrectedToPisces
+{
+
+    static void Main(string[] args)
+    {
+
+    }
+
+} //class
+} //namespace
